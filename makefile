78efb617fb54fa5fbd6406dea20e1cdf6d1124f6
McFlip <gradydenton@yahoo.com>
2017-11-29 17:13:24 -0500

    reading ethernet frames

--- makefile
+++ makefile
@@ -6,9 +6,11 @@ OBJECT = pAnalyzer.o
 SOURCE = pAnalyzer.cpp
 HEADER = pAnalyzer.h
 all:     $(OBJECT) $(OBJECT2)
-	g++ -o $(PROGRAM) $(OBJECT) 2>> log.txt
+# 	g++ -o $(PROGRAM) $(OBJECT) 2>> log.txt
+	g++ -o $(PROGRAM) $(OBJECT)
 $(OBJECT):       $(SOURCE) $(HEADER)
-	g++ $(FLAGS) -o $(OBJECT) -c $(SOURCE) 2>> log.txt
+# 	g++ $(FLAGS) -o $(OBJECT) -c $(SOURCE) 2> log.txt
+	g++ $(FLAGS) -o $(OBJECT) -c $(SOURCE)
 clean:
 	rm -f *.o *.so $(PROGRAM)
 	rm -f log.txt
--- pAnalyzer.cpp
+++ pAnalyzer.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <vector>
+#include <cstdint> //may not need ???
+#include <arpa/inet.h>
 #include "pAnalyzer.h"
 using namespace std;
+
+const int buffsize = 2048;
+
 int main(int argc, char* argv[]){
-  cout << "hello world" << endl;
+  int framecount = 0;
+  uint32_t framesize;
+  ifstream is ("dumpfile.bin", ifstream::binary);
+  char *buffer = new char[buffsize];
+  while(is.read(reinterpret_cast<char *>(&framesize), sizeof(framesize))){
+    buffer[4] = '\0';
+//     framesize = atoi(buffer);
+    printf("framesize: %d \n", framesize);
+//     cout << "framesize: " << framesize  << endl;
+    framesize = htonl(framesize);
+    cout << "framesize(htonl): " << framesize << endl;
+    is.read(buffer, framesize);
+    ++framecount;
+  }
+  cout << "framecount: " << framecount << endl;
   return 0;
 }
